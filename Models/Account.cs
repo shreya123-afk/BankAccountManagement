3f03cd2b1ef0ec1e29727a2162fd3149fdfe72c6
Utkarsh Sen <Utkarsh Sen@LAPTOP-HA9OIJ8A>
2025-02-08 11:37:07 +0000

    Add project files.

---
+++ Models/Account.cs
@@ -0,0 +1,37 @@
+﻿namespace WebApplication1.Models
+{
+    public class Account
+    {
+        /// <summary>
+        /// Initialize the account.
+        /// </summary>
+        /// <param name="accountType"></param>
+        /// <param name="balance"></param>
+        public Account(string accountType, decimal balance)
+        {
+            AccountType = accountType;
+            Balanace = balance;
+        }
+
+        /// <summary>
+        /// Get or set the account id.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// get or set the account balance.
+        /// </summary>
+        public decimal Balanace { get; set; }
+
+        /// <summary>
+        /// get or set the accountType
+        /// </summary>
+        public string AccountType { get; set; }
+
+        /// <summary>
+        /// get or set the User.
+        /// </summary>
+        public User User { get; set; }
+
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,21 @@
+﻿namespace WebApplication1.Models
+{
+    public class User
+    {
+        /// <summary>
+        /// get or set the id.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// get or set name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// get or set the credit rating.
+        /// </summary>
+        public int CreditRating {  get; set; }
+
+    }
+}
